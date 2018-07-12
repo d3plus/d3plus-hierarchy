66da350aa0efcf92cb3a2f7392a05073c5087690
cnavarreteliz <cnavarreteliz@gmail.com>
2018-07-12 12:37:42 -0400

    adds isolation to selected circles in mousemove.shape

--- src/Pack.js
+++ src/Pack.js
@@ -25,9 +25,36 @@ export default class Pack extends Viz {
 
     super();
 
+    this._layoutPadding = 1;
     this._on.mouseenter = () => {};
     this._on["mouseleave.shape"] = () => {
       this.hover(false);
+    };
+    const defaultMouseMove = this._on["mousemove.shape"];
+    this._on["mousemove.shape"] = (d, i) => {
+      defaultMouseMove(d, i);
+      console.log(d);
+      const hoverData = [d];
+      function recursion(d) {
+        if (d.values) {
+          d.values.forEach((h, x) => {
+            hoverData.push(h);
+            recursion (h);
+          });
+        }
+        else {
+          hoverData.push(d);
+        }
+      }
+
+      recursion(d);
+
+      this.hover((h, x) => {
+        if (hoverData.includes(h)) {
+          return true;
+        }
+      });
+
     };
     this._pack = pack();
     this._shape = constant("Circle");
@@ -41,6 +68,7 @@ export default class Pack extends Viz {
     });
     this._sort = (a, b) => b.value - a.value;
     this._sum = accessor("value");
+
   }
 
   /**
@@ -59,7 +87,7 @@ export default class Pack extends Viz {
     let nestedData = nest();
     for (let i = 0; i <= this._drawDepth; i++) nestedData.key(this._groupBy[i]);
     nestedData = nestedData.entries(this._filteredData);
-    
+
     const packData = this._pack
       .padding(this._layoutPadding)
       .size([diameter, diameter])
@@ -68,8 +96,11 @@ export default class Pack extends Viz {
 
     packData.forEach((d, i) => {
       d.__d3plus__ = true;
-      d.id = d.data.key;
+      d.children = d.children;
       d.depth = d.depth;
+      d.i = i;
+      d.id = d.parent ? d.parent.data.key : null;
+      d.parent = d.parent;
       d.x = d.x;
       d.y = d.y;
       if (d.height) d.data.__d3plusOpacity__ = 0.25;
@@ -86,13 +117,32 @@ export default class Pack extends Viz {
           }).node()
         )
         .config({
-          label: d => d.parent && !d.children ? d.data.name : false
+          label: (d, i) => d.parent && !d.children ? this._id(d, i) : false
         })
         .config(configPrep.bind(this)(this._shapeConfig, "shape", "Circle"))
         .render()
     );
 
     return this;
+  }
+
+  hover(_) {
+    this._hover = _;
+    this._shapes.forEach(s => s.hover(_));
+    
+    console.log(this);
+
+    if (this._legend) this._legendClass.hover(_);
+    return this;
+  }
+
+  /**
+      @memberof Pack
+      @desc If *value* is specified, sets the inner and outer padding accessor to the specified function or number and returns the current class instance. If *value* is not specified, returns the current padding accessor.
+      @param {Function|Number} [*value*]
+  */
+  layoutPadding(_) {
+    return arguments.length ? (this._layoutPadding = typeof _ === "function" ? _ : constant(_), this) : this._layoutPadding;
   }
 
   /**
